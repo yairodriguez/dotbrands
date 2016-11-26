724f5fa7bf6dcafbb52efaf50c200ad5ca85f23b
Yair Rodriguez <rodriguezortega.yair@gmail.com>
2016-11-26 00:24:34 -0600

    [maps] Add Complete Mitsubishi Object with coordinates.

---
+++ app/scripts/maps/mitsubishi.js
@@ -0,0 +1,1652 @@
+export default [
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  }, // 1
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  }, // 2
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  }, // 3
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  }, // 4
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  }, // 5
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  }, // 6
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  }, // 7
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  }, // 8
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  }, // 9
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  }, // 10
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  }, // 11
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  }, // 12
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  }, // 13
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  }, // 14
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  }, // 15
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  }, // 16
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  }, // 17
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  }, // 18
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  }, // 19
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  }, // 20
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  }, // 21
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: false,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  },
+  {
+    colour: true,
+  }, // 22
+];
